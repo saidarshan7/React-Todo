af3d88afecb5519bc067f4b25664af92cd757486
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-03-28 18:41:42 +0530

    created al backend routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const zod = require('zod')
 const bodyParser = require('body-parser')
 var jwt = require('jsonwebtoken');
+const {createTodo} = require('./types.js')
+
 app.use(express.json())
 
 
@@ -11,6 +14,19 @@ app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
 app.post("/todo", (req,res)=>{
+const payLoad = req.body;
+const parsePayload = createTodo.safeParse(payLoad);
+
+if(!parsePayload.success) {
+    res.status(411).json({
+        msg: "you sent the wrong inputs",
+    })
+    return;
+
+
+
+}
+
 
 })
 
@@ -20,5 +36,15 @@ app.get("/todo", (req,res)=>{
 
 app.put("/completed", (req,res)=>{
 
+    const payLoad = req.body;
+    const parsePayload = updateTodo.safeParse(payLoad);
+    
+    if(!parsePayload.success) {
+        res.status(411).json({
+            msg: "you sent the wrong inputs",
+        })
+        return;
+    }
+    
 })
 
